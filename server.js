f2d91aadd0363717f55695fec24cc95b533553b3
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-13 00:17:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,18 +11,20 @@ var articleOne = {
     title = 'Article One | Abhishek Bhattacharya',
     heading = 'Article One',
     date= '12 Feb 2017',
-    content =`<p>
-                This is the content for my first article.
-                My IMAD's first html file.
-            </p>
-            <p>
-                This is the content for my first article.
-                My IMAD's first html file.
-            </p>
-            <p>
-                This is the content for my first article.
-                My IMAD's first html file.
-            </p>`
+    content = `
+    <p>
+        This is the content for my first article.
+        My IMAD's first html file.
+    </p>
+    <p>
+        This is the content for my first article.
+        My IMAD's first html file.
+    </p>
+    <p>
+        This is the content for my first article.
+        My IMAD's first html file.
+    </p>
+    `
             
     }; // backquote to use multiple lines of javascript in content
 
