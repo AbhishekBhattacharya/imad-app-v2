e2648e650a4e7197ffd88c0d7cd35f69eb013750
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-14 23:34:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,6 @@
 console.log('Loaded!'); //prints Loaded! on the console.. see inspect element 
 // can write Javascript code directly in Chrome Dev Tools(inspect element)
+
+//Change the text of the main-text div
+var element = document.getElementById("main-text");
+element.innerHTML = "New Value";
