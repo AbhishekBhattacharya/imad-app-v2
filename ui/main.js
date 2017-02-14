5f7222e97ef261e2bdc2b1dd9537733876a4a7f4
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-14 23:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,15 @@
+//Client Side Javascript
+
+
 console.log('Loaded!'); //prints Loaded! on the console.. see inspect element 
 // can write Javascript code directly in Chrome Dev Tools(inspect element)
 
 //Change the text of the main-text div
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//Move the image 
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.style.marginLeft = "100px";
+};
