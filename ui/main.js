1b5acddbf118b2ab69f62c89351e155380865dc1
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-14 23:49:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 
 //Move the image 
-var img = document.getElementById('madi');
+var img = document.getElementById("madi");
 img.onclick = function() {
     img.style.marginLeft = "100px";
 };
