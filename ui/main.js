3d6e9e115b3bef46677a0bb5d2ff623d6dffc53f
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-16 16:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,11 @@ console.log('Loaded!'); //prints Loaded! on the console.. see inspect element
 // can write Javascript code directly in Chrome Dev Tools(inspect element)
 
 //Change the text of the main-text div
-var element = document.getElementById("main-text"); // only one element of index.html should have a  id i.e unique
+var element = document.getElementById('main-text'); // only one element of index.html should have a  id i.e unique
 element.innerHTML = "New Value";
 
 //Move the image 
-var image = document.getElementById("madi");
+var image = document.getElementById('madi');
  image.onclick = function() {
     image.style.marginLeft = "100px";
 };
