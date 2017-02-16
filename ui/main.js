92168d16303df3ac4c813e089f636731838fd361
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-16 16:34:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,7 @@ element.innerHTML = "New Value";
 
 //Move the image 
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10 ;
     img.style.marginLeft = marginLeft + "px";
