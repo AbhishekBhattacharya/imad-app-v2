da0ae1a06e8d4a990ff19bd7f1bc1b6d48b164f8
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-20 22:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ function moveRight() {
     marginLeft = marginLeft + 1 ;
     img.style.marginLeft = marginLeft + "px";
 }
- img.onclick = function () {
+ img.onclick=function(){
    img.style.marginLeft = "100px";
    var interval = setInterval(moveRight,50);//every 50 ms , apply the move right function
 };
