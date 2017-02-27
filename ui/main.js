cb44713d6b568a772592197a892ca95feeb41830
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-02-27 13:57:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,8 @@ function moveRight() {
     marginLeft = marginLeft + 1 ;
     img.style.marginLeft = marginLeft + "px";
 }
- img.onclick=function(){
-   img.style.marginLeft = "100px";
-   var interval = setInterval(moveRight,50);//every 50 ms , apply the move right function
+// img.onclick=function(){
+  // img.style.marginLeft = "100px";
+   //var interval = setInterval(moveRight,50);//every 50 ms , apply the move right function
      
- };
+// };
