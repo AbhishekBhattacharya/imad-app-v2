ea92025b97f33d9b9b10011cdc302fb5966a2cb9
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-03-05 14:30:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ var element = document.getElementById('main-text'); // only one element of index
 element.innerHTML = "New Value";
 
 //Move the image 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1 ;
-    img.style.marginLeft = marginLeft + "px";
-}
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight() {
+  //  marginLeft = marginLeft + 1 ;
+    //img.style.marginLeft = marginLeft + "px";
+//}
 //img.onclick=function(){
   // img.style.marginLeft = "100px";
    //var interval = setInterval(moveRight,50);//every 50 ms , apply the move right function
