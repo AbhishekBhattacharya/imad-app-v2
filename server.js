35b6e8bdc3a95a27a0bfc479f9ec9a50f966a655
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-03-05 15:01:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function(req,res) {
     counter = counter + 1;
-    res.send(counter.toString()); //only send string as a response
+    res.send(counter.toString()); //only allowed to send string as a response
 });
 
 
