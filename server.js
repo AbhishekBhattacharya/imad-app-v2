3ccac5bfe959005591041e8d0d749acc3cafc07b
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-03-11 13:30:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -164,8 +164,8 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-    //SELECT * FROM article WHERE title = 'article-one'
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err ,result) {
+    //SELECT * FROM article WHERE title = '\';DELETE where a = \'asdf' --SQL INJECTION ESCAPED BY BACKSLASH IN FRONT OF SINGLE QUOTE
+    pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function (err ,result) {
       if(err) {
           res.status(500).send(err.toString());
       }  
