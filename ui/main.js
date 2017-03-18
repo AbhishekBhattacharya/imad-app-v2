6cda01e9619dab0518440bee80dc680dc0639ffa
Abhishek Bhattacharya <abhishek.bhattacharya.11@gmail.com>
2017-03-18 17:35:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -86,6 +86,7 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST',"http://abhishekbhattacharya.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username , password:password}));
  
   
